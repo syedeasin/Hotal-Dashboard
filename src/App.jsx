0ce108cb82c202110db0e78133bb28f31289d7b8
syedeasin <syedeasin1000@gmail.com>
2024-08-18 23:55:06 +0600

    sidebar buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Dashboard from "./pages/Dashboard/Dashboard";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Header />
-        <Routes>
-          <Route path={"/"} exact={true} element={<Dashboard />} />
-          <Route path={"/dashboard"} exact={true} element={<Dashboard />} />
-        </Routes>
+        <div className="main d-flex">
+          <div className="sidebarWrapper">
+            {/* <span className="sidebarTitle">main pages</span> */}
+            <Sidebar />
+          </div>
+          <div className="context">
+            <Routes>
+              <Route path={"/"} exact={true} element={<Dashboard />} />
+              <Route path={"/dashboard"} exact={true} element={<Dashboard />} />
+            </Routes>
+          </div>
+        </div>
       </BrowserRouter>
     </>
   );
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,248 @@
+import Button from "@mui/material/Button";
+
+// Icnos
+import { MdChevronRight, MdDashboard } from "react-icons/md";
+
+const Sidebar = () => {
+  return (
+    <>
+      <div className="sidebar ">
+        <ul>
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Dashboard
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Authentication
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Users
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Products
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Products
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Products
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Products
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Products
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Products
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Products
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Products
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Products
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Products
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Products
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Products
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Products
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Products
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>{" "}
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Products
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>{" "}
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Products
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>{" "}
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Products
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>{" "}
+          <li>
+            <Button className="w-100">
+              <span className="icon">
+                <MdDashboard />
+              </span>
+              Products
+              <span className="arrow">
+                <MdChevronRight />
+              </span>
+            </Button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
